be266979c3e992c4188a70c6e9ff6aba0a2614b1
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2007-11-07 00:52:51 +0000

    defined the entry point to framework

---
+++ lib/main.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+# if i'm the main process, then i will spawn a defined number of subprocesses
+if len(sys.argv) == 1:
+    p1 = subprocess.Popen([sys.executable, sys.argv[0], 'render'])
+    print 'created render process %s' % p1
+    p2 = subprocess.Popen([sys.executable, sys.argv[0], 'physics'])
+    print 'created physics process %s' % p2
+    print 'finished main process'
+# if i'm the child process, I have a name
+else:
+    print 'finished %s process' % sys.argv[1]
+
+
+
+
