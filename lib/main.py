98fde6f8ae9bf91cfa7a7f1dc6a89f5f1554d019
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2007-11-07 20:11:42 +0000

    able to start multiple raknet networks from each new process

--- lib/main.py
+++ lib/main.py
@@ -1,19 +1,52 @@
 import subprocess
 import sys
+import pyraknet
+import time
+from network import *
 
+SERVER_PORT = 1000
+
+# Server:
 # if i'm the main process, then i will spawn a defined number of subprocesses
 if len(sys.argv) == 1:
+    # first create the network server in the main process
+    def incoming_connection(packet, s):
+        print 'incoming connection from address: %s' % packet.address
+        print 'incoming connection player: %s' % packet.player
+        print 'incoming connection from address string: %s' % s.rn.get_address_string(packet.address)
+    def incoming_test_packet(packet):
+        print 'incoming packet length: %s' % len(packet.data)
+
+    s = Network()
+    s.set_callback(pyraknet.PacketTypes.ID_NEW_INCOMING_CONNECTION, lambda packet, server=s: incoming_connection(packet, server))
+    s.set_callback(100, incoming_test_packet)
+    s.listen(port=SERVER_PORT, max_players=8)
+    
     p1 = subprocess.Popen([sys.executable, sys.argv[0], 'render'])
     print 'created render process %s' % p1
     p2 = subprocess.Popen([sys.executable, sys.argv[0], 'physics'])
     print 'created physics process %s' % p2
-    p1.wait()
-    p2.wait()
+    
+    # going into main process main loop
+    while True:
+        time.sleep(0.1)
+        s.poll()
+
     print 'finished main process'
+        
+# Client:
 # if i'm the child process, I have a name
 else:
+    # first create the client network
+    c = Network()
+    c.connect(host='localhost', port=SERVER_PORT)
+    
+    # then go into client main loop
+    while True:
+        time.sleep(0.1)
+        c.poll()
+    
     print 'finished %s process' % sys.argv[1]
-
 
 
 
--- lib/network.py
+++ lib/network.py
@@ -1,56 +1,97 @@
-import time
-import struct 
-
 import pyraknet
-from pyraknet import PacketTypes, PacketReliability, PacketPriority
 
-class PeerBase(object):
-        def loop(self):
-                while not self.quit:
-                        time.sleep(0.5)
-                        packet = self.net.receive()
-                        if packet:
-                                self.handle_packet(packet)
+class Error(Exception):
+    pass
 
-class Client(PeerBase):
-        def __init__(self):
-                self.net = pyraknet.Peer()
-                self.net.init(thread_sleep_timer=30)
-                self.net.connect('localhost', 5555)
-                self.quit = False
-                print 'Connecting to server...'
-        def handle_packet(self, packet):
-                packet_type = ord(packet.data[0])
-                if packet_type == PacketTypes.ID_CONNECTION_ATTEMPT_FAILED:
-                        print 'Could not connect. Quitting...'
-                        self.quit = True
-                elif packet_type == PacketTypes.ID_CONNECTION_REQUEST_ACCEPTED:
-                        print 'Connecetd to server!'
-                elif packet_type == PacketTypes.ID_USER_PACKET_ENUM:
-                        print 'Server told me to quit. Quitting...'
-                        self.quit = True
+class SendError(Error):
+    pass
 
-class Server(PeerBase):
-        def __init__(self):
-                self.net = pyraknet.Peer()
-                self.net.init(peers=10, port=5555, thread_sleep_timer=30)
-                self.net.set_max_connections(10)
-                self.quit = False
-                print 'Waiting for a connection'
-        def handle_packet(self, packet):
-                packet_type = ord(packet.data[0])
-                if packet_type == PacketTypes.ID_NEW_INCOMING_CONNECTION:
-                        print 'A client just connected. Sending a quit order.'
-                        data = struct.pack('B', PacketTypes.ID_USER_PACKET_ENUM)
-                        self.net.send(data, len(data), PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE, 0, packet.address)
-                
+class Network(object):
+    def __init__(self):
+        self.rn = pyraknet.Peer()
+        self.callbacks = {}
+        self.default_callback = None
+    def connect(self, host, port, thread_sleep_timer=10):
+        self.rn.init(peers=1, thread_sleep_timer=thread_sleep_timer)
+        self.rn.connect(host=host, port=port)
+        self.default_peer = 0
+    def listen(self, port, max_players, thread_sleep_timer=10):
+        self.rn.init(peers=max_players, port=port, thread_sleep_timer=thread_sleep_timer)
+        self.rn.set_max_connections(max_players)
+    def send(self, data, id=-1, reliability=pyraknet.PacketReliability.RELIABLE_ORDERED, priority=0, channel=0, broadcast=False):
+        '''Send a packet to a peer.
+
+        data: A string of data to send. Set the first byte to be an unused packet type. See pyraknet.PacketTypes.
+        id: The player ID to send to. When broadcast=True, it will send to everyone except this player.
+        '''
+        if id == -1 and not broadcast:
+            try:
+                id = self.default_peer
+            except AttributeError:
+                raise SendError('Tried to send a packet to no one')
+        if id >= 0:
+            address = self.rn.get_address_from_id(id)
+        elif broadcast:
+            address = pyraknet.PlayerAddress()
+        self.rn.send(data, len(data), pyraknet.PacketPriority.LOW_PRIORITY - priority, reliability, channel, address, broadcast)
+    def poll(self):
+        packet = self.rn.receive()
+        while packet:
+            self.process_packet(packet)
+            packet = self.rn.receive()
+    def set_callback(self, packet_type, function):
+        self.callbacks[packet_type] = function
+    def set_default_callback(self, function):
+        self.default_callback = function
+    def del_callback(self, packet_type):
+        del self.callbacks[packet_type]
+    def get_packet_type_name(self, id):
+        for a in dir(pyraknet.PacketTypes):
+            if a[0:3] == 'ID_' and eval('pyraknet.PacketTypes.%s' % (a)) == id:
+                return a
+        return 'ID_UNKNOWN (%i)' % id
+    def process_packet(self, packet):
+        type = ord(packet.data[0])
+        try:
+            self.callbacks[type](packet)
+        except KeyError:
+            try:
+                if self.default_callback:
+                    self.default_callback(packet)
+            except TypeError:
+                name = self.get_packet_type_name(type)
+                print 'Got unhandled packet: %i %s (tried to call default callback)' % (type, name)
+                raise
+
 if __name__ == '__main__':
-        what = raw_input('(C)lient or (S)erver? ')
-        if what[0] == 'C' or what[0] == 'c':
-                me = Client()
-        else:
-                me = Server()
-        me.loop()
+    def incoming_connection(packet):
+        global s
+        print 'incoming connection from address: %s' % packet.address
+        print 'incoming connection player: %s' % packet.player
+        print 'incoming connection from address string: %s' % s.rn.get_address_string(packet.address)
+    def incoming_test_packet(packet):
+        print 'incoming packet length: %s' % len(packet.data)
+    import time
+    s = Network()
+    s.set_callback(pyraknet.PacketTypes.ID_NEW_INCOMING_CONNECTION, incoming_connection)
+    s.set_callback(100, incoming_test_packet)
+    help(s.listen)
+    s.listen(port=1000, max_players=8)
+    count = 5
+    c = {}
+    for i in range(count):
+        c[i] = Network()
+        c[i].connect(host='localhost', port=1000)
+    while 1:
+        time.sleep(0.1)
+        s.poll()
+        data = '%c%s' % (100,  'a' * 1024*1024)
+        for i in range(count):
+            c[i].poll()
+            c[i].send(data, broadcast=True)
+            
+            
+            
         
         
         
