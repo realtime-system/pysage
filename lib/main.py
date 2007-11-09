6dcb1a22dc437e441facd50be89d26be0de74b89
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2007-11-09 22:13:17 +0000

    introducing system which will inherit from message manager.

--- lib/main.py
+++ lib/main.py
@@ -2,29 +2,26 @@ import subprocess
 import sys
 import pyraknet
 import time
-from messaging import *
+from system import *
 from network import *
 
-SERVER_PORT = 8000
+
+class Test(Message):
+    properties = ['name']
+    pass
+
+class Receiver(MessageReceiver):
+    subscriptions = ['Test']
+    def handle_Test(self, msg):
+        # don't consume this message
+        return False
 
 # Server:
 # if i'm the main process, then i will spawn a defined number of subprocesses
 if len(sys.argv) == 1:
-    # first create the network server in the main process
-    def incoming_connection(packet, s):
-        print 'incoming connection from address: %s' % packet.address
-        print 'incoming connection player: %s' % packet.player
-        print 'incoming connection from address string: %s' % s.rn.get_address_string(packet.address)
-    def incoming_test_packet(packet):
-        print 'incoming packet length: %s' % len(packet.data)
-
-    s = Network()
-    s.set_callback(pyraknet.PacketTypes.ID_NEW_INCOMING_CONNECTION, lambda packet, server=s: incoming_connection(packet, server))
-    s.set_callback(100, incoming_test_packet)
-    s.listen(port=SERVER_PORT, max_players=8)
-    
     # create the message manager for the main process
-    messageManager = MessageManager()
+    system = System()
+    system.startMainProcess(8000, 8)
     
     # spawn child processes
     p1 = subprocess.Popen([sys.executable, sys.argv[0], 'render'])
@@ -35,26 +32,21 @@ if len(sys.argv) == 1:
     # going into main process main loop
     while True:
         time.sleep(0.1)
-        s.poll()
+        system.poll()
 
     print 'finished main process'
         
 # Client:
 # if i'm the child process, I have a name
 else:
-    # first create the client network
-    c = Network()
-    c.connect(host='localhost', port=SERVER_PORT)
-    
-    # create the message manager for the child process
-    messageManager = MessageManager()
+    # first create the client system
+    system = System()
+    system.startChildProcess('localhost', 8000, sys.argv[1])
     
     # then go into client main loop
     while True:
         time.sleep(0.1)
-        c.poll()
-    
-    print 'finished %s process' % sys.argv[1]
+        system.poll()
 
 
 
---
+++ lib/system.py
@@ -0,0 +1,50 @@
+# system.py
+# implements a distributed system that uses the messaging functionality
+# and is responsible for local message propagation as well as network messages propagation
+from messaging import *
+from network import *
+
+class System(MessageManager):
+    def init(self):
+        # let the parent message manager init first
+        MessageManager.init(self)
+        self.network = Network()
+        self.network.set_callback(pyraknet.PacketTypes.ID_NEW_INCOMING_CONNECTION, self.incoming_connection)
+        self.network.set_callback(100, self.incoming_test_packet)
+        # give a name
+        self._system_name = ''
+    # first create the network server in the main process
+    def incoming_connection(self, packet):
+        print 'incoming connection from address: %s' % packet.address
+        print 'incoming connection player: %s' % packet.player
+        print 'incoming connection from address string: %s' % self.network.rn.get_address_string(packet.address)
+    def incoming_test_packet(self, packet):
+        print 'incoming packet length: %s' % len(packet.data)
+    def startMainProcess(self, port, max):
+        '''starts the system in main process mode
+            this will make the system act like a "server"
+            return: true if success
+        '''
+        self.network.listen(port=port, max_players=max)
+        self._system_name = 'Main'
+        return True
+    def startChildProcess(self, host, port, name):
+        '''starts the system in child process mode
+            child needs to have a unque name specified
+            return: true if success
+        '''
+        self.network.connect(host=host, port=port)
+        self._system_name = name
+        return True
+    def poll(self):
+        '''a combined tick for the system
+            this should be called as the main loop for each of the process (group) that is created
+            return: true if success
+        '''
+        # first poll the network for messages
+        self.network.poll()
+        # then, tick the internal messaging engine
+        self.tick()
+        return True
+    
+    
