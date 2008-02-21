39f373c5efb02884a98b620c8037f82c59871334
bigjhnny@gmail.com <bigjhnny@gmail.com@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-02-21 06:46:50 +0000

    more metadata in setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,12 @@ from distutils.core import setup
 setup(name='pysage',
       version='1.0',
       packages=['pysage'],
+      
+    # metadata for upload to PyPI
+    author = "John Yang",
+    author_email = "bigjhnny@gmail.com",
+    description = "pySage",
+    license = "creative commons attribution share-alike 1.0",
+    keywords = "python message publisher subscriber",
+    url = "http://code.google.com/p/pysage/"
       )
