5a7fcf28ed3aad6a981ed38fe22b303c5ad56882
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-02-25 07:50:11 +0000

    changing api

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ ez_setup.use_setuptools()
 
 from distutils.core import setup
 setup(name='pysage',
-      version='1.1.4',
+      version='1.1.5',
       packages=['pysage'],
       
     # metadata for upload to PyPI
