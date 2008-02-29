b9100bbef5cabcd2e3f744b8fcaf3284773bbaa3
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-02-29 16:26:21 +0000

    release of 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ ez_setup.use_setuptools()
 
 from distutils.core import setup
 setup(name='pysage',
-      version='1.1.5',
+      version='1.2.0',
       packages=['pysage'],
       
     # metadata for upload to PyPI
