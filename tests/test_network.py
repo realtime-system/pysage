13272ae2fbd2bd775a0ade141c05dd68690147dd
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-11-03 16:40:45 +0000

    adding data packing tests

--- tests/test_network.py
+++ tests/test_network.py
@@ -4,19 +4,24 @@ import unittest
 
 nmanager = ActorManager.get_singleton()
 
-class TestMessage(Message):
+class TestMessage1(Message):
     properties = ['amount']
     types = ['i']
     packet_type = 103
+    
+class TestMessage2(Message):
+    properties = ['size']
+    types = [('i', 'i')]
+    packet_type = 106
     
 class TestReceiver(Actor):
     pass
 
 class TestNetwork(unittest.TestCase):
     def test_packet_creation(self):
-        p = TestMessage(amount=1)
+        p = TestMessage1(amount=1)
     def test_packing(self):
-        p = TestMessage(amount=1)
+        p = TestMessage1(amount=1)
         print p.to_string()
         assert p.to_string() == 'g\x00\x00\x00\x01'
     def test_manager_gid(self):
@@ -24,6 +29,8 @@ class TestNetwork(unittest.TestCase):
     def test_receiver_gid(self):
         r = TestReceiver()
         assert r.gid == (nmanager.gid, id(r))
+    def test_packing_tuple(self):
+        m = TestMessage2(size=(1,1))
 
 
 
