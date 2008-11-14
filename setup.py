974db954c7717485f2b336ac55fbbd0ae0e840a3
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-11-14 23:36:22 +0000

    adding comments and bumping up version number

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 # ez_setup.use_setuptools()
 
 # to deploy, use:
-#   python setup.py register sdist bdist bdist_wininst upload
+#   (does not work ) python setup.py register sdist bdist bdist_wininst upload
+#   python setup.py register sdist upload
+# version numbers:
+#   the version number here is the version number pypi uses, you have to bump it up if upload error occurs
+#   the version number under pysage/__init__.py is the real pysage version number
 
 from distutils.core import setup
 setup(name='pysage',
-      version='1.5.0',
+      version='1.5.1',
       packages=['pysage'],
       
     # metadata for upload to PyPI
